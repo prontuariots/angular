4499a7fc13f6cc9e203cb5b03e91c60df8a2b7a8
Túlio Filho <tuliofilho@avaltech.com.br>
2019-04-19 21:08:23 -0300

    generate uuid

--- src/app/business/registration/registration.service.ts
+++ src/app/business/registration/registration.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { of, Observable } from 'rxjs';
+import { UUID } from 'angular2-uuid';
 
 import { SessionService } from './../../core/session/services/session.service';
 
@@ -30,12 +31,21 @@ export class RegistrationService {
     }
 
     saveCustomer(customer: Customer): void {
+        if (!customer.id)
+            customer.id = UUID.UUID();
+
         this.session.setCustomer(customer);
     }
     saveDoctor(doctor: Doctor): void {
+        if (!doctor.id)
+            doctor.id = UUID.UUID();
+
         this.session.setDoctor(doctor);
     }
     saveUnit(unit: Unit): void {
+        if (!unit.id)
+            unit.id = UUID.UUID();
+
         this.session.setUnit(unit);
     }
 }
